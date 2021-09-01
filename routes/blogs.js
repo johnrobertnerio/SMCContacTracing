118da7dbc4db642ea590b027cb9fd7d6ab0c713a
johnrobertnerio <83899363+johnrobertnerio@users.noreply.github.com>
2021-09-01 19:26:22 +0800

    Add files via upload

---
+++ routes/blogs.js
@@ -0,0 +1,101 @@
+//blog routes
+
+const express = require('express');
+const Blog = require('./../models/Blog');
+const router = express.Router();
+const multer = require('multer');
+
+//define storage for the images
+
+const storage = multer.diskStorage({
+  //destination for files
+  destination: function (request, file, callback) {
+    callback(null, './public/uploads/images');
+  },
+
+  //add back the extension
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+//upload parameters for multer
+const upload = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1024 * 1024 * 3,
+  },
+});
+
+router.get('/new', (request, response) => {
+  response.render('new');
+});
+
+//view route
+router.get('/:slug', async (request, response) => {
+  let blog = await Blog.findOne({ slug: request.params.slug });
+
+  if (blog) {
+    response.render('show', { blog: blog });
+  } else {
+    response.redirect('/');
+  }
+});
+
+//route that handles new post
+router.post('/', upload.single('image'), async (request, response) => {
+  console.log(request.file);
+  // console.log(request.body);
+  let blog = new Blog({
+    fname: request.body.fname,
+    lname: request.body.lname,
+    address: request.body.address,
+    contact: request.body.contact,
+    reason: request.body.reason,
+    confirm: request.body.confirm,
+    img: request.file.filename,
+  });
+
+  try {
+    blog = await blog.save();
+
+    response.redirect(`blogs/${blog.slug}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// route that handles edit view
+router.get('/edit/:id', async (request, response) => {
+  let blog = await Blog.findById(request.params.id);
+  response.render('edit', { blog: blog });
+});
+
+//route to handle updates
+router.put('/:id', async (request, response) => {
+  request.blog = await Blog.findById(request.params.id);
+  let blog = request.blog;
+  blog.fname = request.body.fname;
+  blog.lname = request.body.lname;
+  blog.address = request.body.address;
+  blog.contact = request.body.contact;
+  blog.reason = request.body.reason;
+  blog.confirm = request.body.confirm;
+
+  try {
+    blog = await blog.save();
+    //redirect to the view route
+    response.redirect(`/blogs/${blog.slug}`);
+  } catch (error) {
+    console.log(error);
+    response.redirect(`/seblogs/edit/${blog.id}`, { blog: blog });
+  }
+});
+
+///route to handle delete
+router.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.redirect('/');
+});
+
+module.exports = router;
